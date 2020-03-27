198a812f17d5da40cc730bfd8643bfd6ab8661e5
Matheus F. B. Castegnaro <matheuscastegnaro@hotmail.com>
2020-03-27 14:24:40 -0300

    Criando a interface de log

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi'
 
 import './styles.css';
 import herosImage from '../../assets/heroes.png';
@@ -12,9 +13,12 @@ export default function Logon() {
                 <form>
                     <h1>Faça seu logon </h1>
                     <input placeholder='Sua ID' />
-                    <button type='submit'>Entrar</button>
+                    <button className='button' type='submit'>Entrar</button>
 
-                    <a href="/register"> Não tenho cadastro </a>
+                    <a href="/register">
+                        <FiLogIn size={16} color="#e02041" />
+                        Não tenho cadastro
+                    </a>
                 </form>
             </section>
 
