29319f0f02e5f0b962b5edf0860902e577479a69
Matheus F. B. Castegnaro <matheuscastegnaro@hotmail.com>
2020-03-28 10:08:48 -0300

    Finalizando a aplicaçao front end web

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,24 +1,48 @@
 import React from 'react';
 import { FiLogIn } from 'react-icons/fi'
+import { Link, useHistory } from 'react-router-dom';
+import api from '../../services/api'
 
 import './styles.css';
 import herosImage from '../../assets/heroes.png';
 import logoImage from '../../assets/logo.svg';
+import { useState } from 'react';
 
 export default function Logon() {
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(event) {
+        event.preventDefault();
+
+        try {
+            const response = await api.post('sessions', { id });
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('/profile');
+        } catch (error) {
+            alert('Falha no login, tente novamente mais tarde.');
+        }
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImage} alt="be the hero" />
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu logon </h1>
-                    <input placeholder='Sua ID' />
+                    <input
+                        placeholder='Sua ID'
+                        value={id}
+                        onChange={event => setId(event.target.value)} />
                     <button className='button' type='submit'>Entrar</button>
 
-                    <a href="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#e02041" />
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
 
