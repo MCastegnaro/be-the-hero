4f41252a589901c2c2d382ae7fb7e12fc657dcad
Matheus F. B. Castegnaro <matheuscastegnaro@hotmail.com>
2020-03-31 12:48:44 -0300

    Criando validações para as rotas.

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 const app = express();
+const { errors } = require('celebrate');
 
 // app.use(cors(
 //     origin:'http://meuapp.com'
@@ -10,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.listen(3333, () => {
     console.log('App listening on port 3333!');
